6d044b3bd6a147d18a78cee1c19f774960d981a2
Eduardo Silva <edusilvatele@gmail.com>
2022-08-27 15:42:41 -0300

    Criação e consulta no localstorage

--- js/main.js
+++ js/main.js
@@ -1,19 +1,41 @@
 //capturando o formulario:
 const form = document.getElementById("novoItem");
+ //pegando a lista
+ const lista = document.getElementById('lista');
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach(element => {
+    criaElemento(element);
+});
+//sempre que inicializar a pagina, vai criar os elementos que estao no localStorage;
 
 //capturando o evento do form(submit) => Vamos capturar os dados do formulário quando o botao for clicado;
 form.addEventListener('submit', (event) => {
     //previnindo o comportamento padrão do submit(recarregar);
     event.preventDefault();
     //pegando cada elemento através do objeto elements;
-    const nome = event.target.elements['nome'].value;
-    const quantidade = event.target.elements['quantidade'].value;
+    const nome = event.target.elements['nome'];
+    const quantidade = event.target.elements['quantidade'];
 
-    criaElemento(nome, quantidade);
-})
+        //setando os dados em localStorage:
+    const itemAtual = {
+        'nome': nome.value,
+        'quantidade': quantidade.value
+    }
+    
+    criaElemento(itemAtual);
+
+     itens.push((itemAtual));
+     localStorage.setItem('itens', JSON.stringify(itens));
+
+    //limpando formulario apos criar o elemento:
+    nome.value = "";
+    quantidade.value = "";
+
+    })
 
 //criando uma função para criar um elemento:
-function criaElemento(nome, quantidade){
+function criaElemento(item){
     //Iremos criar um elemento html seguindo o padrão => <li class="item"><strong>7</strong>Camisas</li>
     const novoItem = document.createElement('li'); //criando novo item;
     //adicionando classe ao novo item:
@@ -22,12 +44,11 @@ function criaElemento(nome, quantidade){
     //criando a tag strong:
     const numeroItem = document.createElement('strong');
     //atribuindo o valor da quantidade:
-    numeroItem.innerHTML = quantidade;
+    numeroItem.innerHTML = item.quantidade;
     //para colocar um elemento dentro do outro, é necessario utilizar a função appendChild;
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += item.nome
 
-    //pegando a lista
-    const lista = document.getElementById('lista');
     lista.appendChild(novoItem);
-}
+
+   }
